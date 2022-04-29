972aa1519612d5e7c7d100c5068d7d11d11a7835
jahoperi <97061564+jahoperi@users.noreply.github.com>
2022-04-28 22:15:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit.components.v1 as components
 import requests  # pip install requests
 import plotly.express as px
 
-#import pandas as pd
+import pandas as pd
 #import numpy as np
 
 #import plotly.graph_objects as go 
@@ -30,6 +30,16 @@ page_names = ['Aarón Antonio Nájera Aldape', 'Adriana Guadalupe Gómez Alejand
 
 page = st.radio('Navegación', page_names, index = 0)
 #st.write("**La variable 'page' returns:**", page)
+
+data = pd.read_csv("NDR.csv")
+data = data.set_index('PROFESIONISTAS')
+
+#col1, col2, col3 = st.columns(3)
+
+col1, col2 = st.columns([100, 100])
+
+col1.subheader("DATOS")
+col1.write(data)
 
 
 #1 vertical menu
